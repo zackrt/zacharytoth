ac56375967580ad9c9ebb911a7213808a6df1392
Zack Toth <zacharyrtoth@gmail.com>
2018-07-17 10:28:09 -0700

    changed nav logic

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 	jQuery(function($){
-    	$( '.menu-btn' ).click(function(){
+        let showMenu = false;
+    $( '.menu-btn' ).click(function(){
+        if (!showMenu) {
     		$('.responsive-menu').addClass('expand')
     		$('.menu-btn').addClass('btn-none')
-    	})	
-    	 $( '.close-btn' ).click(function(){
-    		$('.responsive-menu').removeClass('expand')
+        } else {
+            $('.responsive-menu').removeClass('expand')
     		$('.menu-btn').removeClass('btn-none')
-    	})
-  	});
+        }
+        //showmenu starts false, changes to true on click, and the toggling of showMenu changes it between true & false. 
+        showMenu = !showMenu;
+    })	
+});
