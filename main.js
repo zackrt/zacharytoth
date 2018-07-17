f9c737a48ef9448d700b7172ead689e1dd93dc94
Zack Toth <zacharyrtoth@gmail.com>
2018-07-17 10:29:58 -0700

    changed logic

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 	jQuery(function($){
         let showMenu = false;
-    $( '.menu-btn' ).click(function(){
-        if (!showMenu) {
-    		$('.responsive-menu').addClass('expand')
-    		$('.menu-btn').addClass('btn-none')
-        } else {
-            $('.responsive-menu').removeClass('expand')
-    		$('.menu-btn').removeClass('btn-none')
-        }
-        //showmenu starts false, changes to true on click, and the toggling of showMenu changes it between true & false. 
-        showMenu = !showMenu;
-    })	
-});
+            $( '.menu-btn' ).click(function(){
+                if (!showMenu) {
+                    $('.responsive-menu').addClass('expand')
+                    $('.menu-btn').addClass('btn-none')
+                } else {
+                    $('.responsive-menu').removeClass('expand')
+                    $('.menu-btn').removeClass('btn-none')
+                }
+                //showmenu starts false, changes to true on click, and the toggling of showMenu changes it between true & false. 
+                showMenu = !showMenu;
+            })	
+        });
